0c12a0e9a3a6495d7df8adac38de638b03d17f56
VinoVaishnavi <vinovaishnavisr@gmail.com>
2026-06-13 07:10:50 +0530

    configure Firebase for Flutter application

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
@@ -13,7 +20,7 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.blue,
-          title: const Text('Flutter Demo App'),
+          title: const Text('Tuition App'),
         ),
       ),
     );
