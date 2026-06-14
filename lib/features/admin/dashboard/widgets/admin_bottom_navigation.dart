b99b0de6fecd14cbee4d0d87144df22ac808dc05
VinoVaishnavi <vinovaishnavisr@gmail.com>
2026-06-14 23:52:21 +0530

    feat: add AdminBottomNavigation widget for improved navigation in admin dashboard

---
+++ lib/features/admin/dashboard/widgets/admin_bottom_navigation.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:tuition_app/core/constants/app_colors.dart';
+
+class AdminBottomNavigation extends StatelessWidget {
+  const AdminBottomNavigation({
+    super.key,
+    required this.currentIndex,
+    required this.onTap,
+  });
+
+  final int currentIndex;
+  final ValueChanged<int> onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: currentIndex,
+      selectedItemColor: AppColors.primary,
+      onTap: onTap,
+      items: const [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.dashboard_outlined),
+          activeIcon: Icon(Icons.dashboard),
+          label: "Dashboard",
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.school_outlined),
+          activeIcon: Icon(Icons.school),
+          label: "Teacher",
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.people_outline),
+          activeIcon: Icon(Icons.people),
+          label: "Parent",
+        ),
+      ],
+    );
+  }
+}
